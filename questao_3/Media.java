6e46fd11836fcf25b62dfae2d920e6086d215f0d
Camile Marcele <143668626+camimcl@users.noreply.github.com>
2023-09-16 09:17:37 -0300

    Sistema de Aprovação

--- questao_3/Media.java
+++ questao_3/Media.java
@@ -1,4 +1,10 @@
+/*
+3- Faça um programa para a leitura de duas notas parciais de um aluno.   
 
+A mensagem “Aprovado”, se a média alcançada for maior ou igual a sete; 
+A mensagem “Aprovado com Distinção”, se a média for igual a dez; 
+A mensagem “Reprovado” se a média for menor de do que sete; 
+*/
 package questao_3;
 
 public class Media {
