6a6a756f7a6ba0960244109dedff3f848f5b8292
Camile Marcele <143668626+camimcl@users.noreply.github.com>
2023-09-16 09:23:42 -0300

    Posto de Combustível

--- questao_8/Descontos.java
+++ questao_8/Descontos.java
@@ -1,4 +1,17 @@
+/*
+1. Um posto está vendendo combustíveis com a seguinte tabela de descontos: 
+Álcool:
 
+Até 20 litros: desconto de 3% por litro
+Acima de 20 litros: Desconto de 5% por litro.
+Gasolina:
+
+Até 20 litros: desconto de 4% por litro
+Acima de 20 litros, desconto de 6% por litro
+Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível 
+(codificado da seguinte forma: A-álcool. G-gasolina), calcule e imprima o valor a ser pago pelo cliente
+. O valor do combustível será dado pelo desenvolvedor
+*/
 package posto;
 import javax.swing.JOptionPane;
 public class Descontos {
