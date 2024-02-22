45fed9c2c1d28187c707b30e3f9cedc5e5d2a990
Camile Marcele <143668626+camimcl@users.noreply.github.com>
2024-02-22 18:14:25 -0300

    update

--- JavaFX/src/App.java
+++ JavaFX/src/App.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -14,18 +16,25 @@ public class App extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Teste pro trabalho");
-        Button botao = new Button("Clique aqui");
-        botao.setOnAction(new EventHandler<ActionEvent>() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
 
-            @Override
-            public void handle(ActionEvent event) {
-               System.out.println("funciona");
-            }
-            
-        });
-        StackPane root = new StackPane();
-        root.getChildren().addAll(botao);
-        primaryStage.setScene(new Scene(root,300,250));
+        primaryStage.setTitle("Trabalho");
+        primaryStage.setScene(tela);
         primaryStage.show();
+        // Button botao = new Button("Clique aqui");
+        // botao.setOnAction(new EventHandler<ActionEvent>() {
+
+        //     @Override
+        //     public void handle(ActionEvent event) {
+        //        System.out.println("funciona");
+        //     }
+            
+        // });
+        // StackPane root = new StackPane();
+        // root.getChildren().addAll(botao);
+        // primaryStage.setScene(new Scene(root,300,250));
+        // primaryStage.show();
     }
 }
---
+++ JavaFX/src/TelaPrincipalController.java
@@ -0,0 +1,39 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaPrincipalController {
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button insertButton;
+
+    @FXML
+    private Button selectButton;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    void fazerDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fazerInsert(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fazerSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fazerUpdate(ActionEvent event) {
+
+    }
+
+}
